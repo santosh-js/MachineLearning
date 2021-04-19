e86535103f046f36122914a0f8d55db3e40a30d9
Santosh J Sutar <>
2021-04-19 09:37:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# %tensorflow_version 2.x
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compact.v2.feature_column as fc
+import tensorflow as tf
+
+print("Python 2 code")
